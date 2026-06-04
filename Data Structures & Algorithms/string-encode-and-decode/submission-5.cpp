873ae39a7a7a0a262b89667eb3397833f424f95c
scott321654 <38371705+scott321654@users.noreply.github.com>
2026-06-04 17:34:08 +0800

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s;
+        
+        for (int i = 0; i < strs.size(); ++i) {
+            s += to_string(strs[i].size()) + '#' + strs[i];
+        }
+        cout << "decode:"<< s << endl;
+        return s;
+    }
+    
+// "5#hello5#world"
+    vector<string> decode(string s) {
+        vector<string> res;
+        
+        int i = 0;
+        while (i < s.size()) {
+            //find the size of each string
+            int j = i;
+            while (s[j] != '#') {
+                ++j;
+            }
+            
+            int str_size = stoi(s.substr(i, j - i));
+            cout << "str_size:"<< str_size << endl;
+            res.push_back(s.substr(j + 1, str_size));
+            i = j + str_size +1 ;
+        }
+
+        return res;
+
+    }
+};
