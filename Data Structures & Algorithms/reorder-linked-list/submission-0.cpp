668eee585adce7c8c6bde94c2af4a0010933e164
scott321654 <38371705+scott321654@users.noreply.github.com>
2026-07-30 00:36:53 +0800

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        ListNode *next;
+
+        while (curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    };
+
+    void reorderList(ListNode* head) {
+        int length = 0;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* reverseHead  = slow->next;
+        slow->next = nullptr;
+        reverseHead = reverseList(reverseHead);
+        
+        ListNode *first = head;
+        ListNode *second = reverseHead;
+
+        while (second) {
+            ListNode *next1 = first->next;   // 存 first 下一步
+            ListNode *next2 = second->next;  // 存 second 下一步
+
+            first->next = second;   // first 指向 second
+            second->next = next1;   // second 指向原本的 first 下一步
+
+            first = next1;          // first 往前走
+            second = next2;         // second 往前走
+}
+    }
+};
